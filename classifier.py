5746f860a1aaf5f78056c603d60c8ac96b85cd39
Serge Sharoff <serge.sharoff@gmail.com>
2018-11-22 11:33:14 +0000

    results added

--- classifier.py
+++ classifier.py
@@ -15,9 +15,14 @@ import argparse
 
 from keras.preprocessing import sequence
 from keras.models import Model, Input, load_model
-from keras.layers import Dense, Embedding, GlobalMaxPooling1D, SpatialDropout1D, Conv1D, MaxPooling1D, LSTM, Bidirectional, Dropout
+from keras.layers import Dense, Embedding, GlobalMaxPooling1D, SpatialDropout1D, Conv1D, MaxPooling1D, LSTM, Bidirectional, Dropout, concatenate, InputSpec
+
 from keras.optimizers import Adam
 from keras import backend
+
+from keras.engine.topology import Layer
+from keras import initializers as initializers, regularizers, constraints
+from keras import backend as K
 
 from sklearn import metrics
 from sklearn.model_selection import KFold
@@ -101,6 +106,66 @@ if args.gensplit>0:
         print('New doc set is %d' % len(x_train), file=sys.stderr)
 
 x_train = sequence.pad_sequences(x_train, maxlen=args.maxlen)
+
+class AttentionWeightedAverage(Layer):
+    """
+    Computes a weighted average attention mechanism from:
+        Zhou, Peng, Wei Shi, Jun Tian, Zhenyu Qi, Bingchen Li, Hongwei Hao and Bo Xu.
+        “Attention-Based Bidirectional Long Short-Term Memory Networks for Relation Classification.”
+        ACL (2016). http://www.aclweb.org/anthology/P16-2034
+    How to use:
+    see: [BLOGPOST]
+    """
+
+    def __init__(self, return_attention=False, **kwargs):
+        self.init = initializers.get('uniform')
+        self.supports_masking = True
+        self.return_attention = return_attention
+        super(AttentionWeightedAverage, self).__init__(** kwargs)
+
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(ndim=3)]
+        assert len(input_shape) == 3
+
+        self.w = self.add_weight(shape=(input_shape[2], 1),
+                                 name='{}_w'.format(self.name),
+                                 initializer=self.init)
+        self.trainable_weights = [self.w]
+        super(AttentionWeightedAverage, self).build(input_shape)
+
+    def call(self, h, mask=None):
+        h_shape = K.shape(h)
+        d_w, T = h_shape[0], h_shape[1]
+        
+        logits = K.dot(h, self.w)  # w^T h
+        logits = K.reshape(logits, (d_w, T))
+        alpha = K.exp(logits - K.max(logits, axis=-1, keepdims=True))  # exp
+        
+        # masked timesteps have zero weight
+        if mask is not None:
+            mask = K.cast(mask, K.floatx())
+            alpha = alpha * mask
+        alpha = alpha / K.sum(alpha, axis=1, keepdims=True) # softmax
+        r = K.sum(h * K.expand_dims(alpha), axis=1)  # r = h*alpha^T
+        h_star = K.tanh(r)  # h^* = tanh(r)
+        if self.return_attention:
+            return [h_star, alpha]
+        return h_star
+
+    def get_output_shape_for(self, input_shape):
+        return self.compute_output_shape(input_shape)
+
+    def compute_output_shape(self, input_shape):
+        output_len = input_shape[2]
+        if self.return_attention:
+            return [(input_shape[0], output_len), (input_shape[0], input_shape[1])]
+        return (input_shape[0], output_len)
+
+    def compute_mask(self, input, input_mask=None):
+        if isinstance(input_mask, list):
+            return [None] * len(input_mask)
+        else:
+            return None
 
 def createmodel(mname='FT'):
     inp = Input(shape=(args.maxlen, ))
