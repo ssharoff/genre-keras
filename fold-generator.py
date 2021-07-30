7297705e380ebc32355ed3949faa5b8c03d2f6e1
Serge Sharoff <serge.sharoff@gmail.com>
2021-07-30 12:44:29 +0100

    fold-generator

--- fold-generator.py
+++ fold-generator.py
@@ -5,12 +5,17 @@
 # This program is free software under GPL 3, see http://www.gnu.org/licenses/
 # A simple generator of folds for my data
 
-import sys, re, random
+import sys, re, os
 import pandas as pd
 
 import argparse
 
 from sklearn.model_selection import KFold
+
+def createfold(fname,f,fnum,index): # f=train|test
+    with open(f'{fname}/{f}{fnum}.ol', 'w') as fout:
+        for i in index:
+            fout.write(x_train[i])
 
 parser = argparse.ArgumentParser(description="A fold generator")
 parser.add_argument('-p', '--prefix', type=str, default='', help='defaults for embeddings, input files, annotations and dictionaries')
@@ -43,12 +48,10 @@ if args.verbosity>0:
 kf = KFold(n_splits=args.cv_folds, shuffle=True, random_state=args.seed)
 fnum=1
 for train_index, test_index in kf.split(x_train):
-    with open('ftrain'+str(fnum)+'.ol', 'w') as ftrain:
-        for i in train_index:
-            ftrain.write(x_train[i])
-    with open('ftest'+str(fnum)+'.ol', 'w') as ftest:
-        for i in test_index:
-            ftest.write(x_train[i])
-    y_train.iloc[test_index].to_csv('ftest'+str(fnum)+'.csv', sep='\t')
-    y_train.iloc[train_index].to_csv('ftrain'+str(fnum)+'.csv', sep='\t')
+    fname='f'+str(fnum)
+    os.mkdir(fname)
+    createfold(fname,'ftrain',fnum,train_index)
+    createfold(fname,'ftest',fnum,test_index)
+    y_train.iloc[test_index].to_csv(f'{fname}/ftest{fnum}.csv', sep='\t')
+    y_train.iloc[train_index].to_csv(f'{fname}/ftrain{fnum}.csv', sep='\t')
     fnum+=1
